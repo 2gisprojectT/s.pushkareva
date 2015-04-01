45e1d35f1f82db0839538021dcd8bbff4b918093
Pushkareva Svetlana <nelikonya@gmail.com>
2015-04-01 21:53:32 +0600

    Добавлена реакция льва на неизвестный объект

--- 3003_lion/Lion.py
+++ 3003_lion/Lion.py
@@ -16,28 +16,31 @@ class Lion:
     def start(self,state):
         self.state=state
 
-    def view(self,obj):
-         self.obj=obj
-         print "I",self.state, "and I see",self.obj
-         self.newData()
-         print "Now my action is", self.action, "and my state is", self.state,"\n"
-
+    def view(self,obj): #print data
+        if((self.state,obj) in self.table):
+            self.obj=obj
+            print "I",self.state, "and I see",self.obj
+            self.newData()
+            print "Now my action is", self.action, "and my state is", self.state,"\n"
+        else:
+            print "I don't know what is it!\n"
 
     def get_state(self):#next state
         return self.table[(self.state,self.obj)][0]
     def get_action(self):#action
         return self.table[(self.state,self.obj)][1]
 
-    def newData(self):
+    def newData(self):#get action and new state
         self.action=self.get_action()
         new_state=self.get_state()
         if(new_state is not None):
             self.state=new_state
         return
+
 lion=Lion()
 lion.start("fed")
 lion.view("antilope")
 lion.view("antilope")
 lion.view("hunter")
-lion.view("antilope")
-lion.view("tree")
+lion.view("hunter")
+lion.view("cat")
