6c799eae2b0711995bcbd7252055de86c4ec24bc
Pushkareva Svetlana <nelikonya@gmail.com>
2015-04-03 00:28:06 +0600

    Внесены изменения в юнит-тесты

--- 3003_lion/LionTest.py
+++ 3003_lion/LionTest.py
@@ -8,62 +8,65 @@ import unittest
 class LionTest(TestCase):
 
     def test_init(self):
-        obj=Lion()
-        self.assertEqual("",obj.state, 'State is not correct')
-        self.assertEqual("",obj.obj,'Input object is not correct')
-        self.assertEqual("",obj.action, 'Action is not correct')
+        lion=Lion("fed")
+        self.assertEqual("fed",lion.state, 'State is not correct')
+        self.assertEqual("",lion.input_object,'Input object is not correct')
+        self.assertEqual("",lion.action, 'Action is not correct')
 
-    def test_run(self):
-        lion=Lion()
-        lion.start("hungry")
-        self.assertEqual("hungry",lion.state,'State have wrong value')
-        self.assertEqual("",lion.action,'Action is not correct')
-        self.assertEqual("",lion.obj,'Input object is not correct')
+
+
+    def test_get_state(self):
+        lion=Lion("fed")
+        lion.find_obj("antilope")
+        self.assertEqual("hungry",lion.get_state(),'Get_state:State have wrong value')
+        lion.state="hungry"
+        self.assertEqual("fed",lion.get_state(),'Get_state:State have wrong value')
+
+    def test_get_action(self):
+        lion=Lion("fed")
+        lion.find_obj("antilope")
+        self.assertEqual("sleep",lion.get_action(),'Get_action:Action have wrong value')
+        lion.state="hungry"
+        self.assertEqual("eat",lion.get_action(),'Get_action:Action have wrong value')
 
     def test_findAntilope(self):
-         lion=Lion()
-         lion.start("fed")
+         lion=Lion("fed")
          self.assertEqual(True,lion.find_obj("antilope"),'Antilope_find:Return not true')
-         lion.start("hungry")
+         lion=Lion("hungry")
          self.assertEqual(True,lion.find_obj("antilope"),'Antilope_find:Return not true')
 
          lion.find_obj("antilope")
-         self.assertEqual("antilope",lion.obj,'Antilope_find:Input object is not correct')
-
+         self.assertEqual("antilope",lion.input_object,'Antilope_find:Input object is not correct')
 
     def test_findHunter(self):
-         lion=Lion()
-         lion.start("fed")
+         lion=Lion("fed")
          self.assertEqual(True,lion.find_obj("hunter"),'Hunter_find:Return not true')
-         lion.start("hungry")
+         lion=Lion("hungry")
          self.assertEqual(True,lion.find_obj("hunter"),'Hunter_find:Return not true')
 
          lion.find_obj("hunter")
-         self.assertEqual("hunter",lion.obj,'Hunter_find:Input object is not correct')
+         self.assertEqual("hunter",lion.input_object,'Hunter_find:Input object is not correct')
 
     def test_findTree(self):
-         lion=Lion()
-         lion.start("fed")
+         lion=Lion("fed")
          self.assertEqual(True,lion.find_obj("tree"),'Tree_find:Return not true')
-         lion.start("hungry")
+         lion=Lion("hungry")
          self.assertEqual(True,lion.find_obj("tree"),'Tree_find:Return not true')
 
          lion.find_obj("tree")
-         self.assertEqual("tree",lion.obj,'Tree_find:Input object is not correct')
+         self.assertEqual("tree",lion.input_object,'Tree_find:Input object is not correct')
 
     def test_findUnknown(self):
-         lion=Lion()
-         lion.start("fed")
+         lion=Lion("fed")
          self.assertEqual(False,lion.find_obj("frog"),'Unknown_find:Return not true')
-         lion.start("hungry")
+         lion=Lion("hungry")
          self.assertEqual(False,lion.find_obj("frog"),'Tree_find:Return not true')
 
          lion.find_obj("frog")
-         self.assertEqual("",lion.obj,'Unknown_find:Input object is not correct')
+         self.assertEqual("",lion.input_object,'Unknown_find:Input object is not correct')
 
     def test_newDataAntilopeFed(self):
-        lion=Lion()
-        lion.start("fed")
+        lion=Lion("fed")
         lion.find_obj("antilope")
         lion.newData()
 
@@ -71,8 +74,7 @@ class LionTest(TestCase):
         self.assertEqual("sleep",lion.action,'Antilope_fed:Action is not correct')
 
     def test_newDataAntilopeHungry(self):
-        lion=Lion()
-        lion.start("hungry")
+        lion=Lion("hungry")
         lion.find_obj("antilope")
         lion.newData()
 
@@ -80,8 +82,7 @@ class LionTest(TestCase):
         self.assertEqual("eat",lion.action,'Antilope_hungry:Action is not correct')
 
     def test_newDataHunterFed(self):
-        lion=Lion()
-        lion.start("fed")
+        lion=Lion("fed")
         lion.find_obj("hunter")
         lion.newData()
 
@@ -89,8 +90,7 @@ class LionTest(TestCase):
         self.assertEqual("run",lion.action,'Hunter_fed:Action is not correct')
 
     def test_newDataHunterHungry(self):
-        lion=Lion()
-        lion.start("hungry")
+        lion=Lion("hungry")
         lion.find_obj("hunter")
         lion.newData()
 
@@ -98,8 +98,7 @@ class LionTest(TestCase):
         self.assertEqual("run",lion.action,'Hunter_hungry:Action is not correct')
 
     def test_newDataTreeFed(self):
-        lion=Lion()
-        lion.start("fed")
+        lion=Lion("fed")
         lion.find_obj("tree")
         lion.newData()
 
@@ -107,8 +106,7 @@ class LionTest(TestCase):
         self.assertEqual("see",lion.action,'Tree_fed:Action is not correct')
 
     def test_viewTreeHungry(self):
-        lion=Lion()
-        lion.start("hungry")
+        lion=Lion("hungry")
         lion.find_obj("tree")
         lion.newData()
 
