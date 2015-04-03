7361d86333ce31eadf98e0e9658f0d9158030f21
Pushkareva Svetlana <nelikonya@gmail.com>
2015-04-03 21:15:20 +0600

    Убрала print'ы из  view, перенесла newData во view, убрала саму таблицу из класса Lion

--- 3003_lion/Lion.py
+++ 3003_lion/Lion.py
@@ -2,17 +2,11 @@ __author__ = 'neliko'
 
 class Lion:
     input_object=""
-    def __init__(self):
-        self.state="fed"
+    def __init__(self,state,table):
+        self.state=state
         self.action=""
-        self.table={
-            ("hungry","antilope"):("fed","eat"),
-            ("hungry","hunter"):(None,"run"),
-            ("hungry","tree"):(None,"sleep"),
-            ("fed","antilope"):("hungry","sleep"),
-            ("fed","hunter"):("hungry","run"),
-            ("fed","tree"):("hungry","see")
-        }
+        self.table=table
+
     def find_obj(self,input_object):
         if((self.state,input_object) in self.table):
             self.input_object=input_object
@@ -20,14 +14,11 @@ class Lion:
         else:
             return False
 
-
     def view(self,input_object):
-        if(self.find_obj(input_object)):
-            print ("I",self.state, "and I see",self.input_object)
-            self.newData()
-            print ("Now my action is", self.action, "and my state is", self.state,"\n")
-        else:
-            print ("I don't know what is it!\n")
+           self.action=self.get_action()
+           new_state=self.get_state()
+           if(new_state is not None):
+                self.state=new_state
 
     def get_state(self):#next state
         return self.table[(self.state,self.input_object)][0]
@@ -35,18 +26,17 @@ class Lion:
     def get_action(self):#action
         return self.table[(self.state,self.input_object)][1]
 
-    def newData(self):
-        self.action=self.get_action()
-        new_state=self.get_state()
-        if(new_state is not None):
-            self.state=new_state
-        return
-
-
 if __name__ == '__main__':
 
-    # Initialization
-   lion=Lion()
+   table={
+            ("hungry","antilope"):("fed","eat"),
+            ("hungry","hunter"):(None,"run"),
+            ("hungry","tree"):(None,"sleep"),
+            ("fed","antilope"):("hungry","sleep"),
+            ("fed","hunter"):("hungry","run"),
+            ("fed","tree"):("hungry","see")
+        }
+   lion=Lion("fed",table)
    print("Лев сыт")
    while True:
 
@@ -57,6 +47,10 @@ if __name__ == '__main__':
             break
         else:
             print("Реакция Льва:")
-            lion.view(data)
-
+            print ("I",lion.state, "and I see", data)
+            if(lion.find_obj(data)):
+                lion.view(data)
+                print ("Now my action is", lion.action, "and my state is", lion.state,"\n")
+            else:
+                print ("I don't know what is it!\n")
 
