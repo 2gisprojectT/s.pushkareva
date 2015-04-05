e0e9a3aea61611f16234faae9b9480551ab1c42f
Pushkareva Svetlana <nelikonya@gmail.com>
2015-04-05 23:56:43 +0600

    Внесены изменения

--- 3003_lion/Lion.py
+++ 3003_lion/Lion.py
@@ -2,23 +2,20 @@ __author__ = 'neliko'
 
 
 class Lion:
-    input_object = ""
-
     def __init__(self, state, table):
         self.state = state
         self.action = ""
         self.table = table
 
-    def find_obj(self, input_object):
+    def isKnowObject(self, input_object):
         if (self.state, input_object) in self.table:
-            self.input_object = input_object
             return True
         else:
             return False
 
-    def view(self):
-        self.action = self.table[(self.state, self.input_object)][1]
-        new_state = self.table[(self.state, self.input_object)][0]
+    def view(self, input_object):
+        self.action = self.table[(self.state, input_object)][1]
+        new_state = self.table[(self.state, input_object)][0]
         if new_state is not None:
             self.state = new_state
 
@@ -44,8 +41,8 @@ if __name__ == '__main__':
         else:
             print("Реакция Льва:")
             print("I", lion.state, "and I see", data)
-            if lion.find_obj(data):
-                lion.view()
+            if lion.isKnowObject(data):
+                lion.view(data)
                 print("Now my action is", lion.action, "and my state is", lion.state, "\n")
             else:
                 print("I don't know what is it!\n")
--- 3003_lion/LionTest.py
+++ 3003_lion/LionTest.py
@@ -7,87 +7,37 @@ import unittest
 
 class LionTest(TestCase):
     table = {
-        ("hungry", "antelope"): ("fed", "eat"),
-        ("hungry", "hunter"): (None, "run"),
-        ("hungry", "tree"): (None, "sleep"),
-        ("fed", "antelope"): ("hungry", "sleep"),
-        ("fed", "hunter"): ("hungry", "run"),
-        ("fed", "tree"): ("hungry", "see")
+        ("fed", "tree"): ("hungry", "see"),
+        ("hungry", "tree"): (None, "sleep")
     }
 
     def test_init(self):
         lion = Lion("fed", self.table)
 
         self.assertEqual("fed", lion.state, 'Init:State is not correct')
-        self.assertEqual("", lion.input_object, 'Init:Input object is not correct')
         self.assertEqual("", lion.action, 'Init:Action is not correct')
         self.assertEqual(self.table, lion.table, 'Init:Table is not correct')
 
-    def test_findAntelope(self):  # тестирование поиска объекта в таблице
+    def test_isKnowTree(self):  # проверяем, знает ли лев входной объект
         lion = Lion("fed", self.table)
-        self.assertTrue(lion.find_obj("antelope"), 'Antelope_find:Return not true')  # Лев сыт
+        self.assertTrue(lion.isKnowObject("tree"), 'Tree_isKnowObject:Return not true')  # Лев сыт
         lion.state = "hungry"
-        self.assertTrue(lion.find_obj("antelope"), 'Antelope_find:Return not true')  # Лев голоден
+        self.assertTrue(lion.isKnowObject("tree"), 'Tree_isKnowObject:Return not true')  # Лев голоден
 
-        self.assertEqual("antelope", lion.input_object, 'Antelope_find:Input object is not correct')
-
-    def test_findHunter(self):
+    def test_isKnowUnknown(self):
         lion = Lion("fed", self.table)
-        self.assertTrue(lion.find_obj("hunter"), 'Hunter_find:Return not true')
+        self.assertFalse(lion.isKnowObject("frog"), 'Unknown_isKnowObject:Return not true')
         lion.state = "hungry"
-        self.assertTrue(lion.find_obj("hunter"), 'Hunter_find:Return not true')
+        self.assertFalse(lion.isKnowObject("frog"), 'Unknown_isKnowObject:Return not true')
 
-        self.assertEqual("hunter", lion.input_object, 'Hunter_find:Input object is not correct')
-
-    def test_findTree(self):
+    def test_viewTree(self):  # тестирование реакции льва
         lion = Lion("fed", self.table)
-        self.assertTrue(lion.find_obj("tree"), 'Tree_find:Return not true')
-        lion.state = "hungry"
-        self.assertTrue(lion.find_obj("tree"), 'Tree_find:Return not true')
-
-        self.assertEqual("tree", lion.input_object, 'Tree_find:Input object is not correct')
-
-    def test_findUnknown(self):
-        lion = Lion("fed", self.table)
-        self.assertFalse(lion.find_obj("frog"), 'Unknown_find:Return not true')
-        lion.state = "hungry"
-        self.assertFalse(lion.find_obj("frog"), 'Unknown_find:Return not true')
-
-        self.assertEqual("", lion.input_object, 'Tree_find:Input object is not correct')
-
-    def test_viewAntelope(self):  # тестирование реакции льва
-        lion = Lion("fed", self.table)
-        lion.find_obj("antelope")
         # Лев сыт
-        lion.view()
-        self.assertEqual("hungry", lion.state, 'Antelope_fed_view:State is not correct')
-        self.assertEqual("sleep", lion.action, 'Antelope_fed_view:Action is not correct')
-        # Лев голоден
-        lion.view()
-        self.assertEqual("fed", lion.state, 'Antelope_hungry_view:State is not correct')
-        self.assertEqual("eat", lion.action, 'Antelope_hungry_view:Action is not correct')
-
-    def test_viewHunter(self):
-        lion = Lion("fed", self.table)
-        lion.find_obj("hunter")
-
-        lion.view()
-        self.assertEqual("hungry", lion.state, 'Hunter_fed_view:State is not correct')
-        self.assertEqual("run", lion.action, 'Hunter_fed_view:Action is not correct')
-
-        lion.view()
-        self.assertEqual("hungry", lion.state, 'Hunter_hungry_view:State is not correct')
-        self.assertEqual("run", lion.action, 'Hunter_hungry_view:Action is not correct')
-
-    def test_viewTree(self):
-        lion = Lion("fed", self.table)
-        lion.find_obj("tree")
-
-        lion.view()
+        lion.view("tree")
         self.assertEqual("hungry", lion.state, 'Tree_fed_view:State is not correct')
         self.assertEqual("see", lion.action, 'Tree_fed_view:Action is not correct')
-
-        lion.view()
+        # Лев голоден
+        lion.view("tree")
         self.assertEqual("hungry", lion.state, 'Tree_hungry_view:State is not correct')
         self.assertEqual("sleep", lion.action, 'Tree_hungry_view:Action is not correct')
 
